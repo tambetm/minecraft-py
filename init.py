29cf4b38a6935363dce57fd61e43becb97303a29
Tambet Matiisen <tambet.matiisen@gmail.com>
2016-10-15 14:58:19 -0700

    Simpler init.py.

--- init.py
+++ init.py
@@ -1,23 +1,7 @@
 import logging
-
-# create logger
-logger = logging.getLogger('minecraft_py')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
+logging.basicConfig(level=logging.DEBUG)
 
 import minecraft_py
 
-p = minecraft_py.start(10001)
-minecraft_py.stop(p)
+proc, port = minecraft_py.start()
+minecraft_py.stop(proc)
